bde19125c2837182f3e608cbf382bff0050919f2
Elliot Condon <e@elliotcondon.com>
2014-05-22 12:29:54 +1000

    Fixed JS error with previous ACF version
    https://github.com/elliotcondon/acf-field-type-template/issues/19#issuecomment-43714505
    Signed-off-by: Elliot Condon <e@elliotcondon.com>

--- js/input.js
+++ js/input.js
@@ -8,56 +8,64 @@
 	}
 	
 	
-	/*
-	*  ready append (ACF5)
-	*
-	*  These are 2 events which are fired during the page load
-	*  ready = on page load similar to $(document).ready()
-	*  append = on new DOM elements appended via repeater field
-	*
-	*  @type	event
-	*  @date	20/07/13
-	*
-	*  @param	$el (jQuery selection) the jQuery element which contains the ACF fields
-	*  @return	n/a
-	*/
+	if( typeof acf.add_action !== 'undefined' ) {
 	
-	acf.add_action('ready append', function( $el ){
+		/*
+		*  ready append (ACF5)
+		*
+		*  These are 2 events which are fired during the page load
+		*  ready = on page load similar to $(document).ready()
+		*  append = on new DOM elements appended via repeater field
+		*
+		*  @type	event
+		*  @date	20/07/13
+		*
+		*  @param	$el (jQuery selection) the jQuery element which contains the ACF fields
+		*  @return	n/a
+		*/
 		
-		// search $el for fields of type 'FIELD_NAME'
-		acf.get_fields({ type : 'FIELD_NAME'}, $el).each(function(){
+		acf.add_action('ready append', function( $el ){
 			
-			initialize_field( $(this) );
+			// search $el for fields of type 'FIELD_NAME'
+			acf.get_fields({ type : 'FIELD_NAME'}, $el).each(function(){
+				
+				initialize_field( $(this) );
+				
+			});
 			
 		});
 		
-	});
-	
-	
-	/*
-	*  acf/setup_fields (ACF4)
-	*
-	*  This event is triggered when ACF adds any new elements to the DOM. 
-	*
-	*  @type	function
-	*  @since	1.0.0
-	*  @date	01/01/12
-	*
-	*  @param	event		e: an event object. This can be ignored
-	*  @param	Element		postbox: An element which contains the new HTML
-	*
-	*  @return	n/a
-	*/
-	
-	$(document).live('acf/setup_fields', function(e, postbox){
 		
-		$(postbox).find('.field[data-field_type="FIELD_NAME"]').each(function(){
+	} else {
+		
+		
+		/*
+		*  acf/setup_fields (ACF4)
+		*
+		*  This event is triggered when ACF adds any new elements to the DOM. 
+		*
+		*  @type	function
+		*  @since	1.0.0
+		*  @date	01/01/12
+		*
+		*  @param	event		e: an event object. This can be ignored
+		*  @param	Element		postbox: An element which contains the new HTML
+		*
+		*  @return	n/a
+		*/
+		
+		$(document).live('acf/setup_fields', function(e, postbox){
 			
-			initialize_field( $(this) );
-			
+			$(postbox).find('.field[data-field_type="FIELD_NAME"]').each(function(){
+				
+				initialize_field( $(this) );
+				
+			});
+		
 		});
 	
-	});
+	
+	}
 
 
 })(jQuery);
